6cf8f2f68d382cbda2747c3b5555d61aa0d8d22f
Sanuga Kuruppu <128165236+sanuga-lakdinu-kuruppu@users.noreply.github.com>
2025-02-16 09:48:17 +0530

    splash screen completed.
    -dynamic typing added for common scale text with dark mode light mode.
    -dynamic logo added for both dark mode, light mode and different screen sizes.

--- NibmGo/NibmGoApp.swift
+++ NibmGo/NibmGoApp.swift
@@ -1,17 +1,10 @@
-//
-//  NibmGoApp.swift
-//  NibmGo
-//
-//  Created by Kuruppuge Sanuga Lakdinu Kuruppu on 2025-02-14.
-//
-
 import SwiftUI
 
 @main
 struct NibmGoApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            SplashView()
         }
     }
 }
