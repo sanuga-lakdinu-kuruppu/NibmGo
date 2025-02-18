1beb6be4654dfb322208097f7adb0c412c6a4776
Sanuga Kuruppu <128165236+sanuga-lakdinu-kuruppu@users.noreply.github.com>
2025-02-18 22:51:37 +0530

    global navigation added

--- NibmGo/NibmGoApp.swift
+++ NibmGo/NibmGoApp.swift
@@ -2,9 +2,22 @@ import SwiftUI
 
 @main
 struct NibmGoApp: App {
+    @StateObject var globalRouter = NavigationRouter()
     var body: some Scene {
         WindowGroup {
-            SplashView()
+            NavigationStack(path: $globalRouter.pathOutside) {
+                SplashView()
+                    .navigationDestination(for: String.self) { destination in
+                        if destination == "Login" {
+                            LoginView()
+                        } else if destination == "ForgotPasswordVerifyEmail" {
+                            ForgotPasswordVerifyEmailView()
+                        }
+                    }
+            }
+            .accentColor(Color("brandColor"))
+
         }
+        .environmentObject(globalRouter)
     }
 }
