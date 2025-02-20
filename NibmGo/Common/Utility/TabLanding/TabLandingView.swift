15c09335b33c2b5932bbf6f0a684a93ce96d6b2b
Sanuga Kuruppu <128165236+sanuga-lakdinu-kuruppu@users.noreply.github.com>
2025-02-20 13:28:21 +0530

    basic map is done including tab view

---
+++ NibmGo/Common/Utility/TabLanding/TabLandingView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct TabLandingView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+            MapView()
+                .tabItem {
+                    Image(systemName: "map.fill")
+                    Text("Map")
+                }
+                .toolbar(.hidden, for: .navigationBar)
+            FacilityView()
+                .tabItem {
+                    Image(systemName: "building.2.fill")
+                    Text("Facility")
+                }
+            MoreView()
+                .tabItem {
+                    Image(systemName: "ellipsis")
+                    Text("More")
+                }
+        }
+        .navigationBarBackButtonHidden(true)
+        .accentColor(Color("brandColor"))
+    }
+}
+
+#Preview {
+    TabLandingView()
+}
---
+++ NibmGo/Map/Model/MapModel.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+struct MapModel {
+    var mapId: Int
+    var name: String
+    var facilities: [FacilityModel]
+}
